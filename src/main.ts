e60a526ef0a880dd822abb97331f592ddadfee9a
Robert Hardy <rhardy@dcsdk12.org>
2025-10-24 16:10:43 -0600

    Broke out canvas context file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
+import { ctx, canvas, initCanvas } from "./canvas-ctx";
 import "./style.css";
 
-let canvas = document.querySelector<HTMLCanvasElement>("#main-game")!;
-let ctx = canvas.getContext("2d")!;
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+initCanvas();
 
 ctx.fillStyle = "purple";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-function onResize() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	console.log("Resized window", canvas.width, canvas.height);
-}
-
-window.addEventListener("resize", onResize);
