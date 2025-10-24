135c882bdcddce6062dc7421448991396d759375
Robert Hardy <rhardy@dcsdk12.org>
2025-10-24 09:17:54 -0600

    Initialize vite.  Clean up default files.

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "./style.css";
+
+let canvas = document.querySelector<HTMLCanvasElement>("#main-game")!;
+let ctx = canvas.getContext("2d")!;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = "purple";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function onResize() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	console.log("Resized window", canvas.width, canvas.height);
+}
+
+window.addEventListener("resize", onResize);
