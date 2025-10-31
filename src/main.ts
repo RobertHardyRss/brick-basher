936bd6af0c4f51cba6cce85c161704dd99065de9
Robert Hardy <rhardy@dcsdk12.org>
2025-10-31 15:30:29 -0600

    Draw game board

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ctx, canvas, initCanvas } from "./canvas-ctx";
 import { Brick } from "./game-objects/brick";
+import { GameBoard } from "./game-objects/game-board";
 import "./style.css";
 
 initCanvas();
@@ -7,6 +8,8 @@ initCanvas();
 // ctx.fillStyle = "purple";
 // ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-let brick = new Brick(ctx, canvas.width / 2, canvas.height / 2);
-brick.draw();
+// let brick = new Brick(ctx, canvas.width / 2, canvas.height / 2);
+// brick.draw();
 
+let gb = new GameBoard(ctx, canvas.width / 2, 100);
+gb.draw();
