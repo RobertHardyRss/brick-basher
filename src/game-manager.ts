44c15595348cd7ee25a7ae4ef5422ede6a21596f
Robert Hardy <rhardy@dcsdk12.org>
2025-11-10 16:28:03 -0700

    Start adding pattern set slots

--- src/game-manager.ts
+++ src/game-manager.ts
@@ -1,4 +1,7 @@
+import { BRICK_SIZE } from "./constants";
 import { GameBoard } from "./game-objects/game-board";
+import { PatternSlot } from "./game-objects/pattern-slot";
+import { Point } from "./game-objects/point";
 
 export class GameManager {
 	private board: GameBoard;
@@ -7,17 +10,28 @@ export class GameManager {
 		bottom: 50,
 	};
 
+	private slotAlpha!: PatternSlot;
+	private slotBeta!: PatternSlot;
+	private slotCharlie!: PatternSlot;
+
 	constructor(
 		private readonly ctx: CanvasRenderingContext2D,
 		private readonly canvas: HTMLCanvasElement
 	) {
 		this.board = new GameBoard(ctx, canvas.width / 2, this.boardPadding.top);
+
+		let pointBeta = new Point(
+			canvas.width / 2 - BRICK_SIZE * 2,
+			this.boardPadding.top + BRICK_SIZE * 8 + this.boardPadding.bottom
+		);
+		this.slotBeta = new PatternSlot(this.ctx, pointBeta);
 	}
 
 	public draw(): void {
-		const { board } = this;
+		const { board, slotBeta } = this;
 
 		board.draw();
+		slotBeta.brickSet.draw();
 	}
 
 	public update(timestamp: number): void {}
