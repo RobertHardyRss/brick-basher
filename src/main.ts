b1d41d195f8b4121f65ec552d5f511e3914eb3c4
Robert Hardy <rhardy@dcsdk12.org>
2025-12-15 16:21:12 -0700

    Fix build errors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,9 @@ import { GameManager } from "./game-manager";
 initCanvas();
 
 let gm = new GameManager(ctx, canvas);
-let lastTimestamp = 0;
 
-function gameLoop(timestamp: number) {
-	let elapsedTime = timestamp - lastTimestamp;
-	lastTimestamp = timestamp;
-
-	gm.update(elapsedTime);
+function gameLoop() {
+	gm.update();
 	gm.draw();
 
 	// make sure this stays as the last thing
